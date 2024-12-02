afaccac17da9c97fd1daa6e9521a0e4a0405cc18
wings <wings>
2024-12-02 13:42:55 +0800

    完成第一题

--- js/index.js
+++ js/index.js
@@ -45,7 +45,7 @@ function reverseWords(str) {
 findSecondLargest([3, 5, 1, 2, 4]); // 输出: 4
 findSecondLargest([1, 2, 2, 2]); // 输出: null 或 undefined
 function findSecondLargest(arr) {
-    let max = arr[0], max2 = arr[0], max3 = arr[0], index = 0;
+    let max = arr[0], max2 = arr[0], max3 = arr[0], flag = false;
     //找到最大的数
     for (let i = 0; i < arr.length; i++) {
         max = Math.max(max, arr[i]);
@@ -54,7 +54,67 @@ function findSecondLargest(arr) {
     for (let i = 0; i < arr.length; i++) {
         if (arr[i] !== max) {
             max2 = Math.max(max2, arr[i]);
-            index++;
         }
     }
+
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] !== max && arr[i] !== max2) {
+            max3 = Math.max(max3, arr[i]);
+            flag = true;
+        }
+    }
+
+    if (flag) {
+        console.log(max2)
+    } else {
+        console.log('null 或 undefined')
+    }
+}
+
+// 【数组元素的频次统计】编写⼀个函数，接收⼀个数组作为输⼊，并返回⼀个对象，该对象的
+// 键是数组中的元素，值是该元素在数组中出现的频次。
+countFrequencies([1, 2, 2, 3, 3, 3, 4]);
+function countFrequencies(arr) {
+    let obj = {};
+    for (let i = 0; i < arr.length; i++) {
+        if (obj[arr[i]] == undefined) {
+            obj[arr[i]] = 1;
+        } else {
+            obj[arr[i]] += 1;
+        }
+    }
+    console.log(obj);
+    return obj;
+}
+
+// 【数组去重】编写⼀个函数，接受⼀个数组作为输⼊，并返回⼀个去除重复元素后的新数组，
+// 需要两种⽅法。
+
+toUnique1([1, 1, 1, 2, 3, 4, 3, 2]); // [1,2,3]
+toUnique2([1, 1, 1, 2, 3, 4, 3, 2]); // [1,2,3]
+function toUnique1(arr) {
+    let newArr = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (newArr.indexOf(arr[i]) === -1) {
+            newArr.push(arr[i]);
+        }
+    }
+    console.log(newArr);
+    return newArr;
+}
+function toUnique2(arr) {
+    let newArr = [];
+    for (let i = 0; i < arr.length; i++) {
+        let flag = true;
+        for (let j = 0; j < newArr.length; j++) {
+            if (newArr[j] == arr[i]) {
+                flag = false;
+            }
+        }
+        if (flag) {
+            newArr.push(arr[i])
+        }
+    }
+    console.log(newArr);
+    return newArr;
 }
